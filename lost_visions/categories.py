81bb6ec1c155142c6aa36416742f2bd512ffc31c
ian harvey <i.c.harvey@cs.cf.ac.uk>
2015-03-28 16:50:56 +0000

    categories all changed

--- lost_visions/categories.py
+++ lost_visions/categories.py
@@ -57,6 +57,7 @@ DECORATIVE_BORDER_BUTTON_URL = MIKEY_URL + "decorative border.jpg"
 DECORATIVE_MOTIF_BUTTON_URL = MIKEY_URL + "decorativeletter.jpg"
 DECORATIVE_LETTER_BUTTON_URL = MIKEY_URL + "decorativemotif.jpg"
 
+
 class Category():
     def __init__(self, category_id, name, text, img):
         self.img = img
@@ -139,20 +140,20 @@ class CategoryManager():
             .set_answers([100, 200, 303, 400, 500, 600, 800, 1000, 1100]) \
             .set_save(False)
 
-        self.categories[100] = Category('100', 'advert', '', ADVERT_BUTTON_IMAGE_URL) \
-            .set_question('Is there a product/brand name?') \
-            .set_answers([101, 102]).set_synset('advert.n.1')
+        self.categories[100] = Category('100', 'advert', '', ADVERT_BUTTON_IMAGE_URL).set_synset('advert.n.1')
+            # .set_question('Is there a product/brand name?') \
+            # .set_answers([101, 102])
         self.categories[101] = Category('101', 'yes', '', YES_BUTTON_URL) \
             .set_save(False).set_action('product_name_entry')
         self.categories[102] = Category('102', 'no', '', NO_BUTTON_URL).set_save(False)
 
-        self.categories[200] = Category('200', 'building', '', BUILDING_BUTTON_IMAGE_URL)\
-            .set_answers([203, 204]).set_synset('building.n.1')
+        self.categories[200] = Category('200', 'building', '', BUILDING_BUTTON_IMAGE_URL).set_synset('building.n.1')
+            # .set_answers([203, 204])
         # self.categories[201] = Category('201', 'interior', 'Interior?', ICON_URL + 'interior.jpg')
         # self.categories[202] = Category('202', 'exterior', 'Exterior?', ICON_URL + 'exterior.jpg')
-        self.categories[203] = Category('203', 'save', "", YES_BUTTON_URL) \
-            .set_save(False).set_action('building_name_entry')
-        self.categories[204] = Category('204', 'dont_know', "", NO_BUTTON_URL).set_save(False)
+        # self.categories[203] = Category('203', 'save', "", YES_BUTTON_URL) \
+        #     .set_save(False).set_action('building_name_entry')
+        # self.categories[204] = Category('204', 'dont_know', "", NO_BUTTON_URL).set_save(False)
 
         # self.categories[300] = Category('300', 'people', 'People?', ICON_URL + 'people.jpg') \
         #     .set_question('Is this image of an ...').set_answers([301, 302, 303])
@@ -160,10 +161,10 @@ class CategoryManager():
         #     .set_question('Is this a named historical figure?').set_answers([311, 312])
         # self.categories[302] = Category('302', 'group', 'Group?', ICON_URL + 'lv-rect-station.png') \
         #     .set_question('Are there any named historical figures?').set_answers([321, 322])
-        self.categories[303] = Category('303', 'portrait', '', PORTRAIT_BUTTON_IMAGE_URL)\
-            .set_answers([304, 305]).set_synset('portrait.n.2')
-        self.categories[304] = Category('304', "dont_know", "", NO_BUTTON_URL).set_save(False)
-        self.categories[305] = Category('305', 'named_person', "", YES_BUTTON_URL).set_action('person_name_entry')
+        self.categories[303] = Category('303', 'portrait', '', PORTRAIT_BUTTON_IMAGE_URL).set_synset('portrait.n.2')
+            # .set_answers([304, 305])
+        # self.categories[304] = Category('304', "dont_know", "", NO_BUTTON_URL).set_save(False)
+        # self.categories[305] = Category('305', 'named_person', "", YES_BUTTON_URL).set_action('person_name_entry')
 
         self.categories[311] = Category('311', 'no', 'No', NO_BUTTON_URL) \
             .set_question('Any Activities?').set_answers([321, 322]).set_save(False)
@@ -199,48 +200,48 @@ class CategoryManager():
         self.categories[800] = Category('800', 'scientific_drawing', '', SCIENTIFIC_BUTTON_IMAGE_URL) \
             .set_question('Is the illustration ...').set_synset('diagram.n.1') \
             .set_answers([801, 802, 803, 804, 805, 806, 807, 808])
-        self.categories[801] = Category('801', 'Geological', '', GEOLOGICAL_BUTTON_URL) \
-            .set_question('Does the Diagram describe a named location?').set_answers([810, 1])
-        self.categories[810] = Category('810', 'yes', 'Yes', YES_BUTTON_URL).set_save(False).set_answers([1000])
+        self.categories[801] = Category('801', 'Geological', '', GEOLOGICAL_BUTTON_URL).set_synset('geology.n.1') \
+            .set_question('If you can, please name the geological location').set_answers([1001, 1002])
+        # self.categories[810] = Category('810', 'yes', '', YES_BUTTON_URL).set_save(False).set_answers([1000])
 
-        self.categories[802] = Category('802', 'Medical', '', MEDICAL_BUTTON_URL)
-        self.categories[803] = Category('803', 'Engineering', '', ENGINEERING_BUTTON_URL)
-        self.categories[804] = Category('804', 'Botanical', '', BOTANICAL_BUTTON_URL)
-        self.categories[805] = Category('805', 'Zoological', '', ZOOLOGICAL_BUTTON_URL)
-        self.categories[806] = Category('806', 'Archaeological', '', ARCHAEOLOGICAL_BUTTON_URL) \
-            .set_question('Is the archaeological illustration of a named location?').set_answers([816, 1])
-        self.categories[816] = Category('810', 'yes', '', YES_BUTTON_URL).set_save(False).set_answers([1000])
+        self.categories[802] = Category('802', 'Medical', '', MEDICAL_BUTTON_URL).set_synset('medical.a.1')
+        self.categories[803] = Category('803', 'Engineering', '', ENGINEERING_BUTTON_URL).set_synset('engineering.n.2')
+        self.categories[804] = Category('804', 'Botanical', '', BOTANICAL_BUTTON_URL).set_synset('botany.n.2')
+        self.categories[805] = Category('805', 'Zoological', '', ZOOLOGICAL_BUTTON_URL).set_synset('zoology.n.2')
+        self.categories[806] = Category('806', 'Archaeological', '', ARCHAEOLOGICAL_BUTTON_URL).set_synset('archaeology.n.1') \
+            .set_question('Is the archaeological illustration of a named location?').set_answers([1001, 1002])
+        # self.categories[816] = Category('810', 'yes', '', YES_BUTTON_URL).set_save(False).set_answers([1000])
 
-        self.categories[807] = Category('807', 'architectural', '', ARCHITECTURAL_BUTTON_URL) \
-            .set_question('Is the architectural illustration of a named location?').set_answers([817, 1])
-        self.categories[817] = Category('810', 'yes', '', YES_BUTTON_URL).set_save(False).set_answers([1000])
+        self.categories[807] = Category('807', 'Architectural', '', ARCHITECTURAL_BUTTON_URL).set_synset('architecture.n.2') \
+            .set_question('If you can, please name the location of the architectural illustration').set_answers([1001, 1002])
+        # self.categories[817] = Category('810', 'yes', '', YES_BUTTON_URL).set_save(False).set_answers([1000])
         self.categories[808] = Category('808', 'no', '', NONE_OF_THESE_BUTTON_URL).set_save(False)
 
         # self.categories[900] = Category('900', 'music', 'Musical Score?', ICON_URL + 'music.jpg')
 
         self.categories[600] = Category('600', 'map', '', MAP_BUTTON_IMAGE_URL).set_synset('map.n.1') \
             .set_question('If you can, please name the mapped location')\
-            .set_answers([1001, 1002, 1004]).set_synset('map.n.1')
+            .set_answers([1001, 1002]).set_synset('map.n.1')
 
         self.categories[1000] = Category('1000', 'location', '', LOCATION_BUTTON_IMAGE_URL) \
-            .set_save(False).set_question('Can you describe the location?').set_answers([1001, 1002, 1004])
+            .set_save(False).set_question('If you can, please name the location').set_answers([1001, 1002])
 
         # self.categories[1200] = Category('1200', 'ethnographic', 'Travel/ Ethnography?', ICON_URL + 'travel.jpg')
         # self.categories[1300] = Category('1300', 'words', 'Handwritten Text?', ICON_URL + 'words.jpg')
 
-        self.categories[1001] = Category('1001', 'Name', 'By Name?', ICON_URL + 'lv-rect-station.png')\
+        self.categories[1001] = Category('1001', 'Name', '', BYNAME_BUTTON_URL)\
             .set_action('gazetteer').set_save(False).set_answers([1003, 1004])
-        self.categories[1002] = Category('1002', 'Map', 'On a map?', ON_A_MAP_BUTTON_URL)\
+        self.categories[1002] = Category('1002', 'Map', '', ON_A_MAP_BUTTON_URL)\
             .set_save(False).set_action('map').set_answers([1004])
         self.categories[1003] = Category('1003', 'save', '', ICON_URL + 'tick.png').set_save(False)
         self.categories[1004] = Category('1004', 'back', '', BACK_BUTTON_URL).set_save(False)
 
         self.categories[1100] = Category('1100', 'literature', '', LITERATURE_BUTTON_IMAGE_URL) \
-            .set_save(False).set_synset('illustration.n.1')\
+            .set_save(True).set_synset('literature.n.3')\
             .set_question('Does this illustrate...').set_answers([1101, 1102, 1103, 406])
-        self.categories[1101] = Category('1101', 'poem', '', POEM_BUTTON_URL)
-        self.categories[1102] = Category('1102', 'play', '', PLAY_BUTTON_URL)
-        self.categories[1103] = Category('1103', 'novel', '', NOVEL_BUTTON_URL)
+        self.categories[1101] = Category('1101', 'poem', '', POEM_BUTTON_URL).set_synset('poetry.n.2')
+        self.categories[1102] = Category('1102', 'play', '', PLAY_BUTTON_URL).set_synset('play.n.2')
+        self.categories[1103] = Category('1103', 'novel', '', NOVEL_BUTTON_URL).set_synset('novel.n.1')
 
     def get_tag_for_category_id(self, category_id):
         try:
@@ -257,9 +258,14 @@ class CategoryManager():
 
     def get_category_data(self, cat_id):
         root = False
+        move_on = False
 
         # If the category doesn't have any children return children for root
         parent_category = self.categories.get(int(cat_id))
+
+        if len(parent_category.answer_ids) == 0:
+            move_on = True
+
         answer_categories = []
         actions = []
         if parent_category is None or len(parent_category.answer_ids) == 0 or int(cat_id) == 0:
@@ -299,4 +305,8 @@ class CategoryManager():
                 pass
         # We return the question from the parent category
         # if click Person, and get asked "name the person"
-        return {'question': parent_category.question, 'answers': answer_categories, 'actions': actions, 'root': root}
+        return {'question': parent_category.question,
+                'answers': answer_categories,
+                'actions': actions,
+                'next_question': move_on,
+                'root': root}
